4d877e0b4b9ce52e083303564a4651ecfd3c7ea3
Emmie Salama <emmie.salama@gmail.com>
2015-07-20 09:14:10 -0400

    added pages

--- app.js
+++ app.js
@@ -26,9 +26,6 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use("/views", function (req, res) {
-  res.render(req.path);
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
